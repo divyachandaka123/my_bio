44a18689be196d15df8459329d4290a4f4a56078
Mourya C Chowdary Nalagatla -X (mnalagat - MINDTREE LIMITED at Cisco) <mnalagat@partnet.cisco.com>
2020-11-18 15:15:59 +0530

    Updates

---
+++ precache-manifest.cd5658e1d298ee81ca6b33d473f28c6e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546a96a68e686508bd58e85538a18a9a",
+    "url": "/my_bio/index.html"
+  },
+  {
+    "revision": "cc4c37c56bf8df77f387",
+    "url": "/my_bio/static/css/main.6d333111.chunk.css"
+  },
+  {
+    "revision": "9bf04535fdc36eea2b91",
+    "url": "/my_bio/static/js/2.1c04f1e3.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/my_bio/static/js/2.1c04f1e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4c37c56bf8df77f387",
+    "url": "/my_bio/static/js/main.94424a99.chunk.js"
+  },
+  {
+    "revision": "1f2417215bf735bb94d7",
+    "url": "/my_bio/static/js/runtime-main.942eb437.js"
+  },
+  {
+    "revision": "48ae6434de48325549277b49f7ebf731",
+    "url": "/my_bio/static/media/arrow.48ae6434.svg"
+  },
+  {
+    "revision": "681afa2ed6c043ea96751e7b755cc561",
+    "url": "/my_bio/static/media/bolt.681afa2e.svg"
+  },
+  {
+    "revision": "e4f2d6b028d93b8c7988d2132bf13ca2",
+    "url": "/my_bio/static/media/github.e4f2d6b0.svg"
+  },
+  {
+    "revision": "23a7dee5995afbb2f9ad3b32799c6f82",
+    "url": "/my_bio/static/media/gmail.23a7dee5.svg"
+  },
+  {
+    "revision": "133ce3f86906cf46c6618abb24212cfe",
+    "url": "/my_bio/static/media/home.133ce3f8.svg"
+  },
+  {
+    "revision": "8241a4d1ee30c8af04d4504c1c403087",
+    "url": "/my_bio/static/media/imagee.8241a4d1.png"
+  },
+  {
+    "revision": "46ce74c2e7da57b99857891a9e0ebf94",
+    "url": "/my_bio/static/media/instagram.46ce74c2.svg"
+  },
+  {
+    "revision": "d849bc2557228a905cb46a21f1819d90",
+    "url": "/my_bio/static/media/linkedin.d849bc25.svg"
+  }
+]);
